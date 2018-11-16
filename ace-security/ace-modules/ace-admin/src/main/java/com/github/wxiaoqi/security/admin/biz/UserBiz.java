388457b66e69532cce92a2b2dcde7db465ba77d6
Jackjunzhang <2427902512@qq.com>
2018-11-16 15:41:37 +0800

    ace-security

---
+++ ace-security/ace-modules/ace-admin/src/main/java/com/github/wxiaoqi/security/admin/biz/UserBiz.java
@@ -0,0 +1,58 @@
+package com.github.wxiaoqi.security.admin.biz;
+
+import com.ace.cache.annotation.Cache;
+import com.ace.cache.annotation.CacheClear;
+import com.github.wxiaoqi.security.admin.entity.User;
+import com.github.wxiaoqi.security.admin.mapper.GateLogMapper;
+import com.github.wxiaoqi.security.admin.mapper.MenuMapper;
+import com.github.wxiaoqi.security.admin.mapper.UserMapper;
+import com.github.wxiaoqi.security.auth.client.jwt.UserAuthUtil;
+import com.github.wxiaoqi.security.common.biz.BaseBiz;
+import com.github.wxiaoqi.security.common.constant.UserConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author wanghaobin
+ * @create 2017-06-08 16:23
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserBiz extends BaseBiz<UserMapper,User> {
+    @Autowired
+    private UserAuthUtil userAuthUtil;
+    @Autowired
+    private MenuMapper menuMapper;
+    @Autowired
+    private GateLogMapper gateLogMapper;
+    @Override
+    public void insertSelective(User entity) {
+        String password = new BCryptPasswordEncoder(UserConstant.PW_ENCORDER_SALT).encode(entity.getPassword());
+        entity.setPassword(password);
+        super.insertSelective(entity);
+    }
+
+    @Override
+    @CacheClear(pre="user{1.username}")
+    public void updateSelectiveById(User entity) {
+        super.updateSelectiveById(entity);
+    }
+
+    /**
+     * 根据用户名获取用户信息
+     * @param username
+     * @return
+     */
+    @Cache(key="user{1}")
+    public User getUserByUsername(String username){
+        User user = new User();
+        user.setUsername(username);
+        return mapper.selectOne(user);
+    }
+
+
+}
